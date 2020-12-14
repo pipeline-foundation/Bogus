7bc264c26371b6187854cab7148b5be00bfd36a3
Svetoslav Zlatkov <62066501+szlatkow@users.noreply.github.com>
2020-12-14 11:28:14 +0200

    Feature/devsecops (#1)
    Initial DevSecOps workflow with GitHub Actions
    
    Add SonarSource validation
    Add test result publishing to Testspace
    Add CD release sequence

--- Source/Bogus/Properties/AssemblyInfo.cs
+++ Source/Bogus/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Bogus Fake Data Generator for .NET")]
 [assembly: AssemblyProduct("Bogus")]
 [assembly: AssemblyCompany("Brian Chavez")]
-[assembly: AssemblyCopyright("Brian Chavez © 2015")]
+[assembly: AssemblyCopyright("Brian Chavez © 2020")]
 [assembly: AssemblyVersion("0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0-localbuild built on 1/1/2015 12:00:00 AM")]
+[assembly: AssemblyInformationalVersion("0.0.0-localbuild built on 12/1/2020 3:11:33 PM")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescription("https://github.com/bchavez/Bogus")]
 [assembly: InternalsVisibleTo("Bogus.Tests")]
@@ -18,10 +18,10 @@ namespace System {
         internal const System.String AssemblyTitle = "Bogus Fake Data Generator for .NET";
         internal const System.String AssemblyProduct = "Bogus";
         internal const System.String AssemblyCompany = "Brian Chavez";
-        internal const System.String AssemblyCopyright = "Brian Chavez © 2015";
+        internal const System.String AssemblyCopyright = "Brian Chavez © 2020";
         internal const System.String AssemblyVersion = "0.0.0";
         internal const System.String AssemblyFileVersion = "0.0.0";
-        internal const System.String AssemblyInformationalVersion = "0.0.0-localbuild built on 1/1/2015 12:00:00 AM";
+        internal const System.String AssemblyInformationalVersion = "0.0.0-localbuild built on 12/1/2020 3:11:33 PM";
         internal const System.String AssemblyTrademark = "MIT License";
         internal const System.String AssemblyDescription = "https://github.com/bchavez/Bogus";
         internal const System.String InternalsVisibleTo = "Bogus.Tests";
